e7d36b4aa56ab61b88d112a12e081e4e3157d6ba
Lucas <lucas.lebars@edu.univ-fcomte.fr>
2025-03-14 09:58:33 +0100

    base fix

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,5 +1,5 @@
 import PocketBase from "pocketbase";
-const pb = new PocketBase("http://127.0.0.1:8090");
+const pb = new PocketBase("https://sae203.lucas-lebars.fr:443");
 
 export async function allPartenaires() {
     const records = await pb.collection('Partenaires').getFullList() ;
